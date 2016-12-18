3990f4cbf740d48834d11f1c6f933e131627f1a2
MajorMaad <quentin.audinot@gmail.com>
2016-12-18 13:56:44 +0100

    Setup env port

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const express = require('express');
 
 let app = express();
 
+// Middlewares
+app.set('port', process.env.PORT || 8080);
+
 app.get('/', function(req, res) {
     res.json({
         'message': 'Hello world'
     });
 });
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000');
+app.listen(app.get('port'), function() {
+    console.log('Listening on port ' + app.get('port'));
 });

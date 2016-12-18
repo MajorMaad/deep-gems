46d7ea9f32eb950b65a84256e5867f07c35f989a
MajorMaad <quentin.audinot@gmail.com>
2016-12-18 14:23:38 +0100

    Setting up middlewares

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
 const routes = require('./app/Http/routes');
 
 let app = express();
 
 // Middlewares
 app.set('port', process.env.PORT || 8080);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', routes);
 
 app.get('/', function(req, res) {
+    console.log(req.cookies);
     res.json({
         'message': 'Hello world'
     });
